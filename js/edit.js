3166a71ce5329cc87d966145360889426b102d41
akagisho <sho@ohkun.com>
2012-06-21 22:15:55 +0900

    added common.js.

--- js/edit.js
+++ js/edit.js
@@ -90,19 +90,3 @@ function getCookie(host) {
         }
     });
 }
-
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('#!/') + 3).split('&');
-    for (var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
-
-function getPage() {
-    location.href = 'get.html#!/host=' + $('#host').val();
-}
-
--- js/get.js
+++ js/get.js
@@ -27,38 +27,3 @@ function getCookie(host) {
         $('#cookie').val(result);
     });
 }
-
-function editPage() {
-    location.href = 'edit.html#!/host=' + $('#host').val();
-}
-
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('#!/') + 3).split('&');
-    for (var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
-
-function indent(data) {
-    data = data.replaceAll('{"', '{' + "\n\t\t" + '"');
-    data = data.replaceAll('},{', "\n\t" + '},' + "\n\t" + '{');
-    data = data.replaceAll(',"', ',' + "\n\t\t" + '"');
-    data = data.replace(/:/g, ': ');
-    data = data.replace(/^\[{/, '[' + "\n\t" + '{');
-    data = data.replace(/}\]$/, "\n\t" + '}' + "\n" + ']');
-
-    return data;
-}
-
-String.prototype.replaceAll = function(str1,str2){
-    var temp = this;
-    while (temp.indexOf(str1) != -1) {
-        temp=temp.replace(str1,str2);
-    }
-    return temp;
-};
-
